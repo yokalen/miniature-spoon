191746ad6ff8a95bdc552094c16467ed56b6d593
My Computer <yokalen@gmail.com>
2024-01-19 11:12:09 -0800

    working

--- js/main.js
+++ js/main.js
@@ -88,13 +88,15 @@ const inventors = [
   // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
   // // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
   // mw-category
-  // const links = [...document.querySelectorAll('.mw-category a')]
-  // const de = links.map(link => link.innerText).filter(links.includes('de'))
+  // const links = [...document.querySelectorAll('.mw-category a')];
+  // const de = links.map(link => link.innerText().filter(links.includes('de')));
+ 
   // console.log(links)
   
   // 7. sort Exercise
   // Sort the people alphabetically by last name
   
+
   // 8. Reduce Exercise
   // Sum up the instances of each of these
   const data = [
